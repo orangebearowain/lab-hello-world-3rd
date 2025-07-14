b4998cd5b47276cd328c17c6f2bf0124e8574c63
orangebearowain <yanowain2010@gmail.com>
2025-07-13 23:01:31 -0500

    tic tac toe

--- main.py
+++ main.py
@@ -7,11 +7,7 @@ import tic_tac_toe_board
 app = FastAPI()
 
 r = redis.Redis(
-    host="ai.thewcl.com",        
-    port=6379,                   
-    password="atmega328",
-    db=12,         
-    decode_responses=True        
+      
     )
 
 class MoveRequest(BaseModel):
